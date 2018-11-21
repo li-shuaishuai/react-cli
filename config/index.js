2dce705e9e7ceccf1eaa9f1fa6b5aab95a5e6055
lishuaishuai <lishuaishuai.it@gmail.com>
2018-11-21 14:26:49 +0800

    update config and lib

--- config/index.js
+++ config/index.js
@@ -9,9 +9,8 @@ const CONFIG = {
   projectName: 'react-cli',
   authorID: 'li-shuaishuai', // github ID
   templateName: 'react-template', // 模板名称
-  npmReg: 'https://registry.npmjs.org/',
-  templateVersion: '1.0.0', // 项目初始化版本号
-  ignoreList: []
+  branch: 'feature/handlebars', // 下载模板的分支
+  npmReg: 'https://registry.npmjs.org/'
 }
 
 module.exports.CONFIG = CONFIG
