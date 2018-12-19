16916250f8a23540917cddd3d35bef71f3fb5fe8
lishuaishuai <lishuaishuai.it@gmail.com>
2018-12-19 09:05:26 +0800

    modify templates

--- templates/component/index.jsx
+++ templates/component/index.jsx
@@ -5,7 +5,7 @@ import * as styles from './{{componentName}}.{{cssPre}}'
 export default class {{componentNameUP}} extends React.Component {
   render() {
     return (
-      <div className={styles.container}>
+      <div className={styles.wrapper}>
         {{componentName}}
       </div>
     )
